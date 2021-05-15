d048edac40ceb01392db2a82c7de6e0bae304eb3
Patrick Bartels <patrick.bartels@gmail.com>
2021-05-15 11:15:40 +0200

    Re-add validations for customers

--- app/models/hinzufugen.rb
+++ app/models/hinzufugen.rb
@@ -1,8 +1,8 @@
 class Hinzufugen < ApplicationRecord
   self.table_name = "customers"
-  # validates :first_name,:presence => {:message => "Vorname can't be blank." }
-  # validates :last_name,:presence => {:message => "Nachname can't be blank." }
-  # validates :email,:presence => {:message => "E-Mail-Adresse can't be blank." },
-  #           :uniqueness => {:message => "E-Mail-Adresse already exists."}
+  validates :first_name,:presence => {:message => "Vorname can't be blank." }
+  validates :last_name,:presence => {:message => "Nachname can't be blank." }
+  validates :email,:presence => {:message => "E-Mail-Adresse can't be blank." },
+            :uniqueness => {:message => "E-Mail-Adresse already exists."}
 
 end
